2b6d53ec51c9d4dcf6be3967732e0661c165f9cb
Guilherme <guilhermebauer16@gmail.com>
2023-05-31 18:43:41 -0300

    add funcao de add tarefas

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import sqlite3
+from time import sleep
+banco = sqlite3.connect('listaDeTarefas.db')
+cursor = banco.cursor()
+# cursor.execute("CREATE TABLE Tarefas ('tarefa' text ,'horário' integer , 'dia da semana' text ) ")
+
+def leiaInt(msg):
+
+
+    while True:
+        numero = input(msg)
+        try:
+            int_numero = int(numero)
+            break
+        except ValueError:
+            print('Por favor digite um número para continuar!')
+
+    return int_numero
 
 while True:
     print('/='* 20)
@@ -8,28 +26,39 @@ while True:
 [2]Remover uma tarefa
 [3]Ver suas tarefas
 [4]Sair ''')
+    print('/=' * 20)
+
+
+    opcao = leiaInt('sua opção:')
 
     print('/=' * 20)
-    while True:
-       opcao = input('Digite sua opção: ')
-       try:
-           int_opcao = int(opcao)
-           break
-       except ValueError:
-           print('Por favor digite um número')
-
-    if int_opcao == 1:
+    if opcao == 1:
+        sleep(1)
         print('Nova tarefa')
+        print('/=' * 20)
+        tarefa = input('tarefa: ')
+        horario = leiaInt('horario: ')
+        semana = input('Dia da semana: ')
+        cursor.execute("INSERT INTO Tarefas VALUES(?,?,?)",(tarefa,horario,semana))
+        banco.commit()
 
-    elif int_opcao == 2:
+    elif opcao == 2:
+        sleep(1)
         print('Deletar tarefa')
-    
-    elif int_opcao == 3:
-        print('ver suas tarefas')
 
-    elif int_opcao == 4:
+    elif opcao == 3:
+        sleep(1)
+        print('ver suas tarefas')
+        print('/=' * 20)
+        cursor.execute("SELECT * FROM Tarefas")
+        print(cursor.fetchall())
+        sleep(1)
+
+    elif opcao == 4:
         print('saindo...')
+        sleep(1)
         break
 
-    elif int_opcao == 0 or int_opcao > 4:
+    else:
         print('Por favor digite uma opção valida!')
+banco.close()
